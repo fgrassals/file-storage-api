a60dd3d7f5167c099038897762c4648a5ed99535
Franklin Grassals <fgrassals@gmail.com>
2020-07-12 10:45:38 -0400

    Create the User entity, repository and the corresponding sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+# username: test, password: test
+insert into users (username, password) values ('test', '$2a$10$/9iZqwhMOwhtZyhq0edzteESqErGMglHGpG8/U8gssc5ZuT/Kx0/G');
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,8 @@
+create table users
+(
+    id               int         not null primary key auto_increment,
+    username         varchar(20) not null unique,
+    password         varchar(64) not null,
+    created_at       datetime    not null default current_timestamp,
+    last_modified_at datetime on update current_timestamp
+);
