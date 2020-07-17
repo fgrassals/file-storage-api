06bd302deee167df5769fbbedf0a5cec2b061853
Franklin Grassals <fgrassals@gmail.com>
2020-07-17 12:58:04 -0400

    Fix the data.sql comments using non sql syntax

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-# username: test, password: test
+/* username: test, password: test */
 insert into users (username, password) values ('test', '$2a$10$/9iZqwhMOwhtZyhq0edzteESqErGMglHGpG8/U8gssc5ZuT/Kx0/G');
-# username: test2, password: test
+/* username: test2, password: test */
 insert into users (username, password) values ('test2', '$2a$10$/9iZqwhMOwhtZyhq0edzteESqErGMglHGpG8/U8gssc5ZuT/Kx0/G');
